882356804aa0d2e61f8c5ce0ace861e22ed54a16
Rod <sixtycycles@gmail.com>
2022-05-13 14:05:14 -0700

    Create sign_the_book.py

---
+++ sign_the_book.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+with open ("/home/epss_check/scripts/last_check" , 'r') as check:
+    num = check.readlines()
+    check.close()
+
+with open ("/home/epss_check/scripts/last_check", "w") as checkin:
+    checkin.writelines(f"still here! {datetime.now()}")
+    print("checked in")
