0643b1fdc5b42d80e3921da75d1d607a048681a7
Rod <sixtycycles@gmail.com>
2022-06-11 16:04:28 -0700

    somtimes files get deleted.

--- sign_the_book.py
+++ sign_the_book.py
@@ -1,14 +1,13 @@
 """
-leave this on the server that is off site, serving as "check in"  this opens a file
+leave this on the server that is off site, serving as "check in host"
 """
-
 from datetime import datetime
-# was going to do a check here, but didn't. lose this: 
-# with open ("/home/epss_check/scripts/last_check" , 'r') as check:
-#     num = check.readlines()
-#     check.close()
 
-with open ("/home/epss_check/scripts/last_check", "w") as checkin:
+with open ("/home/epss_check/scripts/last_check" , 'r') as check:
+    num = check.readlines()
+    check.close()
+
+with open ("/home/epss_check/scripts/last_check", "w+") as checkin:
     checkin.writelines(f"still here! {datetime.now()} \n")
     print("checked in")
     checkin.close()
