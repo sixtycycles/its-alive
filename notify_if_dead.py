4ee3de1705f8624ae3be694cd80071e4c03201a2
Rod <sixtycycles@gmail.com>
2022-06-11 16:08:50 -0700

    Update notify_if_dead.py

--- notify_if_dead.py
+++ notify_if_dead.py
@@ -1,5 +1,5 @@
 """
-You gotta change the numbers. This should run via cron/systemd every minute, set the "max_delay" value to the number of minutes it can be dead for before yelling about it. 
+Put your own token and stuff in here. This should run via cron/systemd every minute, set the "max_delay" value to the number of minutes it can be dead for before yelling about it. 
 """
 
 from twilio.rest import Client
@@ -9,14 +9,14 @@ from datetime import datetime, timedelta
 auth_token  = "HAHAHAHAHAHAHAHAHANOPE"
 account_sid = "NOPENOPENOPENOPENOPENOPE"
 twilio_phone_num = +12223334455
-notify_phone_num = +11234567890"
+notify_phone_num = +11234567890
 
 client = Client(account_sid, auth_token)
 file_mod_time = datetime.fromtimestamp(os.stat('/home/epss_check/scripts/last_check').st_mtime)
 now = datetime.today()
 max_delay = timedelta(minutes=15)
 
-if now-file_mod_time > max_delay:
+if now - file_mod_time > max_delay:
     message = client.messages.create(
         to=f"{notify_phone_num}",
         from_=f"{twilio_phone_num}",
